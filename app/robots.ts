d47b4817d28e66e7c083c6bdc5da841e2acff6c6
Tashir Khan <khantashir05@gmail.com>
2026-05-18 11:05:51 +0530

    fixed

--- app/robots.ts
+++ app/robots.ts
@@ -2,19 +2,10 @@ import { MetadataRoute } from 'next'
 import { siteConfig } from '@/config/site'
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl =
-    process.env.NEXT_PUBLIC_SITE_URL || siteConfig.url
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || siteConfig.url
 
   return {
     rules: [
-      {
-        userAgent: 'facebookexternalhit',
-        allow: '/',
-      },
-      {
-        userAgent: 'Facebot',
-        allow: '/',
-      },
       {
         userAgent: '*',
         allow: '/',
