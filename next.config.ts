ab8fc8a31276a178809beb68b37d4ad3d9991ff2
Tashir Khan <khantashir05@gmail.com>
2026-05-20 23:05:17 +0530

    fixed

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,22 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "tgn-nine.vercel.app",
+          },
+        ],
+        destination: "https://www.groundnarrative.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
